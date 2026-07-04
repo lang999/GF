3093abe4dd776607e7497677d06dcd5496bf3830
FGBLH <pengli08211@gmail.com>
2026-07-05 05:01:33 +0800

    Update 奇优影院.py

--- py/奇优影院.py
+++ py/奇优影院.py
@@ -23,8 +23,7 @@ class Spider(Spider):
             "电影": "1",
             "电视剧": "2", 
             "动漫": "3",
-            "综艺": "4",
-            "理论片": "8"
+            "综艺": "4"
         }
         classes = [{'type_name': k, 'type_id': v} for k, v in cateManual.items()]
         result['class'] = classes
